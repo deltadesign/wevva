a84d789b457218252a85ce655e1c843270fd094f
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2020-10-14 14:07:30 +0100

    Updates

---
+++ precache-manifest.11589c81bee0f6121fe96a3e309701f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649fbeac55516b30867a039f6a99b7b2",
+    "url": "/wevva/index.html"
+  },
+  {
+    "revision": "fae3ee1524a2c90abf6a",
+    "url": "/wevva/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "a59775f5d50681ad3f1f",
+    "url": "/wevva/static/js/2.249e24eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wevva/static/js/2.249e24eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae3ee1524a2c90abf6a",
+    "url": "/wevva/static/js/main.25386d81.chunk.js"
+  },
+  {
+    "revision": "2548c17f3310b866e28a",
+    "url": "/wevva/static/js/runtime-main.0a3df01e.js"
+  }
+]);
