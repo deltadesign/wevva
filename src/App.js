a597c4d31ad7d59c72781c793b14835443d1e211
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2020-10-14 13:08:47 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from'axios'
 
-function App() {
+// a0a4ed2421e783072dfb96623b63f774
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  componentDidMount(){
+    let response = fetch('https://api.openweathermap.org/data/2.5/weather?q=sheffield,uk&appid=a0a4ed2421e783072dfb96623b63f774')
+    console.log(response)
+    response.then((response)=> {
+      response.json().then((jsonresponse)=>{
+        this.setState({
+          weather: jsonresponse.weather
+        })
+      })
+    })
+  }
+
+  render () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <pre>{JSON.stringify(this.state)}</pre>
+
+
+    <ul>
+      <h2>Todo</h2>
+      <li>add api call</li>
+      <li>get 5 day forecast</li>
+      <li>add some stuff</li>
+    </ul>
+
+    </>
   );
+  }
 }
 
 export default App;
