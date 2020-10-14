144425e2ab8b7d68491e524edf9d53e0405e48b2
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2020-10-14 17:49:50 +0100

    no clue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import './App.css';
-import axios from'axios'
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Sheffield,uk&appid=0fbd6c6079d3fdfcb32d21d2c735760c';
+// import axios from'axios'
 
-// a0a4ed2421e783072dfb96623b63f774
+
+// api id and gh page
+// 0fbd6c6079d3fdfcb32d21d2c735760c
+// https://deltadesign.github.io/wevva/
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      isLoaded: false,
+      weather: ""
 
     }
   }
 
+  
   componentDidMount(){
-    let response = fetch('https://api.openweathermap.org/data/2.5/weather?q=sheffield,uk&appid=a0a4ed2421e783072dfb96623b63f774')
-    console.log(response)
+    let response = fetch(url); // fetches data from api
+    console.log(response) //console logs response from api as promise
+
     response.then((response)=> {
-      response.json().then((jsonresponse)=>{
+      response.json().then((jsonresponse) => {
         this.setState({
+          isLoaded: true,
           weather: jsonresponse.weather
         })
       })
@@ -26,18 +35,24 @@ class App extends React.Component {
 
   render () {
   return (
-    <>
-    <pre>{JSON.stringify(this.state)}</pre>
+    <div className = "app">
+
+      <pre>STATE  = {JSON.stringify(this.state)}</pre>
 
 
-    <ul>
-      <h2>Todo</h2>
-      <li>add api call</li>
-      <li>get 5 day forecast</li>
-      <li>add some stuff</li>
-    </ul>
+       {this.state.isLoaded ? "" : "<<<< ▲ LOADING ▲ >>>>"} {/* Loading check */}
 
-    </>
+      
+        <h2>Todo's</h2>
+        <br />
+        add api call
+        <br />
+        get 5 day forecast
+        <br />
+        add some stuff
+      
+    
+    </ div>
   );
   }
 }
